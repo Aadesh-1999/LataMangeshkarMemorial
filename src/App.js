322a7b4236111838b151afd94d4f59011b843e02
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-07 23:57:24 +0530

    Basic components added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Learn React
+          RIP Lata Mangeshkar
       </header>
       <Footer/>
     </div>
