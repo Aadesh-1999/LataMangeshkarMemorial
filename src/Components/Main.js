3ad16abe13521f645ec2b0824c3f77bdf57df193
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-09 03:34:02 +0530

    Background Image and Typing Effect Name added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,12 +7,12 @@ export const Main = () => {
         <Header />
         <div className='factsAndAcheivements row d-flex justify-content-center'>
             <div className="facts col d-flex justify-content-center p-5 row">
-                <h3 className='py-3 col-sm col-lg-6'><q>One of the best-known and Most respected playback singers in India</q></h3><br /><hr />
-                <h3 className='py-3 col-sm col-lg-6'><q>Eight-decade long career of playback singing</q></h3><br /><hr />
-                <h3 className='py-3 col-sm col-lg-6'><q>First voice of Indian film and the most recorded singing artist in history.</q></h3><br /><hr />
-                <h3 className='py-3 col-sm col-lg-6'><q>Lata Mangeshkar last recorded a song in 2019</q></h3><br /><hr />
-                <h3 className='py-3 col-sm col-lg-6'><q>Got Featured in Guiness Book Of World Record</q></h3><br /><hr />
-                <h3 className='customLink py-3 col-sm col-lg-6'><Link to='/facts'><q>Know More Facts About Lata Ji...</q></Link></h3><br /><hr />
+                <h3 className='py-3 col'><q>One of the best-known and Most respected playback singers in India</q></h3><br /><hr />
+                <h3 className='py-3 col'><q>Eight-decade long career of playback singing</q></h3><br /><hr />
+                <h3 className='py-3 col'><q>First voice of Indian film and the most recorded singing artist in history.</q></h3><br /><hr />
+                <h3 className='py-3 col'><q>Lata Mangeshkar last recorded a song in 2019</q></h3><br /><hr />
+                <h3 className='py-3 col'><q>Got Featured in Guiness Book Of World Record</q></h3><br /><hr />
+                <h3 className='customLink py-3 col'><Link to='/facts'><q>Know More Facts About Lata Ji...</q></Link></h3><br /><hr />
             </div>
         </div>
     </div>);
