a7c56cc71daff6bac798d393db4f5a9f57a243bc
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-09 01:11:02 +0530

    Background Image and Typing Effect Name added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Footer from './Components/Footer';
+import {Footer} from './Components/Footer';
 import './App.css';
+import { Header } from './Components/Header';
+import {Navbar} from './Components/Navbar';
+import { Routes, Route,BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          RIP Lata Mangeshkar
-      </header>
-      <Footer/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<><Navbar/><Header/><Footer/></>}/>
+        
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/Header.css'
+
+export const Header = () => {
+  return (<div>
+      <div className="App-header pb-5">
+        <h1>The Nightingale Of India..!!</h1>
+      </div>
+  </div>);
+};
