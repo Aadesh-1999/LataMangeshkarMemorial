ff31d8c8c02400aa16b534b5cd2311f3a01273d7
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-09 03:21:45 +0530

    Background Image and Typing Effect Name added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import {Footer} from './Components/Footer';
 import './App.css';
-import { Header } from './Components/Header';
 import {Navbar} from './Components/Navbar';
 import { Routes, Route,BrowserRouter as Router } from "react-router-dom";
+import { Main } from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Routes>
-        <Route path="/" element={<><Navbar/><Header/><Footer/></>}/>
+        <Route path="/" element={<><Navbar/><Main/><Footer/></>}/>
         <Route path="*" element={<><h1>Page Not Found</h1></>}/>
       </Routes>
       </Router>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Header } from './Header';
-
+import './styles/Main.css';
+import { Link } from "react-router-dom";
 export const Main = () => {
-  return (<div>
-      <Header/>
-  </div>);
+    return (<div className='row'>
+        <Header />
+        <div className='factsAndAcheivements row d-flex justify-content-center'>
+            <div className="facts col d-flex justify-content-center p-5 row">
+                <h3 className='py-3'><q>One of the best-known and Most respected playback singers in India</q></h3><br /><hr />
+                <h3 className='py-3'><q>Eight-decade long career of playback singing</q></h3><br /><hr />
+                <h3 className='py-3'><q>First voice of Indian film and the most recorded singing artist in history.</q></h3><br /><hr />
+            </div>
+            <div className="acheivements col d-flex justify-content-center align-items-center p-5 row">
+                <h3 className='py-3 d-flex  align-items-center'><q>Lata Mangeshkar last recorded a song in 2019</q></h3><br /><hr />
+                <h3 className='py-3 d-flex  align-items-center'><q>Got Featured in Guiness Book Of World Record</q></h3><br /><hr />
+                <h3 className='customLink py-3 d-flex justify-content-start align-items-center'><Link to='/facts'><q>Know More Facts About Lata Ji...</q></Link></h3><br /><hr />
+            </div>
+        </div>
+    </div>);
 };
