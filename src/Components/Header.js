c64e8baced51cf24e9f0825aca7677e26ec3f048
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-09 02:36:09 +0530

    Background Image and Typing Effect Name added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,9 @@ import './styles/Header.css'
 
 export const Header = () => {
   return (
+    <>
       <div className="App-header pb-5">
         <h1>The Nightingale Of India..!!</h1>
-      </div>);
+      </div>
+    </>);
 };
