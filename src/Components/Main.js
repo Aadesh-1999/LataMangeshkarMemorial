f1141c3b59781424adf3d5cb9ca733a9f0a2faf9
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-09 17:17:11 +0530

    Background Image and Typing Effect Name added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,16 +3,16 @@ import { Header } from './Header';
 import './styles/Main.css';
 import { Link } from "react-router-dom";
 export const Main = () => {
-    return (<div className='row'>
+    return (<div >
         <Header />
-        <div className='factsAndAcheivements row d-flex justify-content-center'>
-            <div className="facts col d-flex justify-content-center p-5 row">
-                <h3 className='py-3 col'><q>One of the best-known and Most respected playback singers in India</q></h3><br /><hr />
-                <h3 className='py-3 col'><q>Eight-decade long career of playback singing</q></h3><br /><hr />
-                <h3 className='py-3 col'><q>First voice of Indian film and the most recorded singing artist in history.</q></h3><br /><hr />
-                <h3 className='py-3 col'><q>Lata Mangeshkar last recorded a song in 2019</q></h3><br /><hr />
-                <h3 className='py-3 col'><q>Got Featured in Guiness Book Of World Record</q></h3><br /><hr />
-                <h3 className='customLink py-3 col'><Link to='/facts'><q>Know More Facts About Lata Ji...</q></Link></h3><br /><hr />
+        <div className='factsAndAcheivements d-flex justify-content-center'>
+            <div className="facts d-flex justify-content-center p-5 row">
+                <h3 className='col-12 col-md-6 py-3'><q>One of the best-known and Most respected playback singers in India</q><hr/></h3>
+                <h3 className='col-12 col-md-6 py-3'><q>Eight-decade long career of playback singing</q><hr/></h3>
+                <h3 className='col-12 col-md-6 py-3'><q>First voice of Indian film and the most recorded singing artist in history.</q><hr/></h3>
+                <h3 className='col-12 col-md-6 py-3'><q>Lata Mangeshkar last recorded a song in 2019</q><hr/></h3>
+                <h3 className='col-12 col-md-6 py-3'><q>Got Featured in Guiness Book Of World Record</q><hr/></h3>
+                <h3 className='col-12 col-md-6 customLink py-3'><Link to='/facts'><q>Know More Facts About Lata Ji...</q></Link><hr/></h3>
             </div>
         </div>
     </div>);
