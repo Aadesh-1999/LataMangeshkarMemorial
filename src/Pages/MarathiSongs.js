fb000d4c109e41f7bc2c3979a7ff3204b427641c
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-13 01:30:11 +0530

    minor changes

--- src/Pages/MarathiSongs.js
+++ src/Pages/MarathiSongs.js
@@ -3,13 +3,13 @@ import React from 'react';
 export const MarathiSongs = () => {
   return (<div className='container row d-flex justify-content-center align-items-center'>
       <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/H4-TtMhOJyg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/vHZnbf6CeYE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div className="col-12 p-3 col-md-6">
       <iframe width="100%" height="250px" src="https://www.youtube.com/embed/YxA1oYyGzwI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div className="col p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/Mt0nTlWfQlk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/tN2ctZl-7tg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div className="col-12 p-3 col-md-6">
       <iframe width="100%" height="250px" src="https://www.youtube.com/embed/k4B2q86np7k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
@@ -21,7 +21,7 @@ export const MarathiSongs = () => {
       <iframe width="100%" height="250px" src="https://www.youtube.com/embed/OrIHndYsIT8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/vHZnbf6CeYE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/H4-TtMhOJyg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div className="col-12 p-3 col-md-6">
       <iframe width="100%" height="250px" src="https://www.youtube.com/embed/LlGkRxbn-N4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
@@ -29,6 +29,5 @@ export const MarathiSongs = () => {
       <div className="col-12 p-3 col-md-6">
       <iframe width="100%" height="250px" src="https://www.youtube.com/embed/UPL9AzZi85g" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
-      
   </div>);
 };
