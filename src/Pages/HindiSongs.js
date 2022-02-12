e0c7c60ccb1dc13d16d010566f33826e76aaabc7
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-13 01:15:53 +0530

    Added songs

--- src/Pages/HindiSongs.js
+++ src/Pages/HindiSongs.js
@@ -3,28 +3,28 @@ import React from 'react';
 export const HindiSongs = () => {
   return (<div className='container row m-5'>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/7Ums6XFisaQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/7Ums6XFisaQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/I92DErnN4Mk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/I92DErnN4Mk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/amCp3EPPjUs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/amCp3EPPjUs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/-NCdeldQLFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/-NCdeldQLFc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/2kAwKm8Ztqs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/2kAwKm8Ztqs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/qOkE3ekdaco" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/qOkE3ekdaco" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/TgiXNUessiA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/TgiXNUessiA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     <div className="col-12 p-3 col-md-6">
-      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/nBMXwgw7tbc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="250px" src="https://www.youtube.com/embed/nBMXwgw7tbc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   </div>);
 };
--- src/Pages/Songs.js
+++ src/Pages/Songs.js
@@ -4,10 +4,10 @@ import { NavLink, Outlet } from "react-router-dom";
 export const Songs = () => {
   return (<div className="d-flex justify-content-center align-items-start row">
     <div className="col-12 row d-flex justify-content-center">
-      <div className='m-5 col d-flex justify-content-center'>
+      <div className='my-5 col d-flex justify-content-center'>
         <NavLink className='btn px-5 py-3 shadow border' to='/songs/marathi'>Marathi</NavLink>
       </div>
-      <div className='m-5 col d-flex justify-content-center'>
+      <div className='my-5 col d-flex justify-content-center'>
         <NavLink className='btn px-5 py-3 shadow border' to='/songs/hindi'>Hindi</NavLink>
       </div>
     </div>
